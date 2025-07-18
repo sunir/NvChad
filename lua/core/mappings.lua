8fc26e8db1adc83a4ff067baea3ffa4172efb15f
sunir <sunir@bibdex.com>
2025-07-18 16:27:13 -0400

    added quicklist bindings <leader>qj,qj,qo,qc

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -33,6 +33,12 @@ M.general = {
     ["<leader>n"] = { "<cmd> set nu! <CR>", "Toggle line number" },
     ["<leader>rn"] = { "<cmd> set rnu! <CR>", "Toggle relative number" },
 
+    -- quickfix navigation
+    ["<leader>qj"] = { "<cmd> cnext <CR>", "Next quickfix item" },
+    ["<leader>qk"] = { "<cmd> cprev <CR>", "Previous quickfix item" },
+    ["<leader>qo"] = { "<cmd> copen <CR>", "Open quickfix list" },
+    ["<leader>qc"] = { "<cmd> cclose <CR>", "Close quickfix list" },
+
     -- Allow moving the cursor through wrapped lines with j, k, <Up> and <Down>
     -- http://www.reddit.com/r/vim/comments/2k4cbr/problem_with_gj_and_gk/
     -- empty mode is same as using <cmd> :map
